40f6e830d0c951adf3bc8afbdc213c976af4e263
attwad <userattwad@server.fake>
2013-07-27 11:24:21 +0900

    added setup script, uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+    test_extras = {
+        'test_suite': 'pythonosc.test',
+    }
+except ImportError:
+    from distutils.core import setup
+    test_extras = {}
+
+
+setup(
+    name='python-osc',
+    version='1.0',
+    author='attwad',
+    author_email='tmusoft@gmail.com',
+    description=(
+        'Open Sound Control server and client implementations in pure Python'),
+    long_description=open('README.rst').read(),
+    url='https://github.com/attwad/python-osc',
+    platforms='any',
+    packages=[
+        'pythonosc',
+        'pythonosc.parsing',
+        'pythonosc.test',
+        'pythonosc.test.parsing',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: System :: Networking',
+    ],
+    **test_extras
+)
+
