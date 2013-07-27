93a8b0066f84b20803cf8ed41afc18f1a9f09066
attwad <userattwad@server.fake>
2013-07-27 11:12:20 +0900

    added simple server eexample

---
+++ examples/simple_server.py
@@ -0,0 +1,31 @@
+import argparse
+import math
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def print_volume_handler(args, volume):
+  print("[{0}] ~ {1}".format(args[0], volume))
+
+def print_compute_handler(args, volume):
+  try:
+    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+  except ValueError: pass
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip",
+      default="127.0.0.1", help="The ip to listen on")
+  parser.add_argument("--port",
+      type=int, default=5005, help="The port to listen on")
+  args = parser.parse_args()
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/debug", print)
+  dispatcher.map("/volume", print_volume_handler, "Volume")
+  dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+  server = osc_server.ThreadingOSCUDPServer(
+      (args.ip, args.port), dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
