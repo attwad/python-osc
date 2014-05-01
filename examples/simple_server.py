752c1bdfd1c254412591c277259fcf747b5fa04f
attwad <userattwad@server.fake>
2014-05-01 18:24:47 +0900

    made the example compatible with the newly returned address param

--- examples/simple_server.py
+++ examples/simple_server.py
@@ -4,10 +4,10 @@ import math
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-def print_volume_handler(args, volume):
+def print_volume_handler(unused_addr, args, volume):
   print("[{0}] ~ {1}".format(args[0], volume))
 
-def print_compute_handler(args, volume):
+def print_compute_handler(unused_addr, args, volume):
   try:
     print("[{0}] ~ {1}".format(args[0], args[1](volume)))
   except ValueError: pass
